586951bcbe6a3194937ed44bae6790c5c5cf1758
igor.zhuley <jtahun@bk.ru>
2023-08-08 00:56:01 +0600

    HW is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	// Place your code here.
+	startString := "Hello, OTUS!"
+	reversedString := reverseString(startString)	
+	fmt.Println(reversedString)
+}
+
+func reverseString(input string) string {	
+	runes := []rune(input)	
+	var reversed strings.Builder
+	for i := len(runes) - 1; i >= 0; i-- {
+		reversed.WriteRune(runes[i])
+	}
+	return reversed.String()
 }
